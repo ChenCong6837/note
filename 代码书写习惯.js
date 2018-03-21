ea72b10736010e99db3478670cec9228fb55c5e0
chencong <337812462@qq.com>
2018-03-21 21:52:17 +0800

    4.3 空格

--- 代码书写习惯.js
+++ 代码书写习惯.js
@@ -2,7 +2,7 @@
  * @Author: ChenCong 
  * @Date: 2018-03-13 18:55:11 
  * @Last Modified by: ChenCong
- * @Last Modified time: 2018-03-21 21:34:58
+ * @Last Modified time: 2018-03-21 21:48:35
  */
 
 //参考自：http://alloyteam.github.io/CodeGuide/#project-naming
@@ -1001,13 +1001,13 @@
 
 4.2 分号
     以下几种情况后需加分号：
-        (1) 变量声明；
-        (2) 表达式；
-        (3) return；
-        (4) throw；
-        (5) break；
-        (6) continue；
-        (7) do-while；
+        (1) 变量声明
+        (2) 表达式
+        (3) return
+        (4) throw
+        (5) break
+        (6) continue
+        (7) do-while
 
         // var declaration
         var x = 1;
@@ -1020,8 +1020,78 @@
             x++;
         } while (x < 10);
 
+4.3 空格
+    以下几种情况不需要空格：
+        (1) 对象的属性名后
+        (2) 前缀一元运算符后
+        (3) 后缀一元运算符前
+        (4) 函数调用括号前
+        (5) 无论是函数声明还是函数表达式，'('前不要空格
+        (6) 数组的'['后和']'前
+        (7) 对象的'['后和']'前
+        (8) 运算符的'('后和')'前
+    以下几种情况需要空格：
+        (1) 二元运算符前后
+        (2) 三元运算符'?:'前后
+        (3) 代码块'{'前
+        (4) 下列关键字前：else，while，catch，finally
+        (5) 下列关键字后：if，else，for，while，do，switch，case，try，catch，finally，with，return，typeof
+        (6) 单行注释'//'后（若单行注释和代码同行，则'//'前也需要），多行注释'*'后
+        (7) 对象的属性值前
+        (8) for循环，分号后留一个空格，前置条件如果有多个，逗号后留一个空格
+        (9) 无论是函数声明还是函数表达式，'{'前一定要有空格
+        (10) 函数的参数之间
+            
+            // not good
+            var a = {
+                b :1
+            };
 
- 
+            // good
+            var a = {
+                b: 1
+            };
+
+            // not good
+            ++ x;
+            y ++;
+            z = x?1:2;
+
+            // good
+            ++x;
+            y++;
+            z = x ? 1 : 2;
+
+            // not good
+            var a = [ 1, 2 ];
+
+            // good
+            var a = [1, 2];
+
+            // not good
+            var a = ( 1+2 )*3;
+
+            // good
+            var a = (1 + 2) * 3;
+
+            // no space before '(', one space before '{', one space between function parameters
+            var doSomething = function(a, b, c) {
+                // do something
+            };
+
+            // no space before '('
+            doSomething(item);
+
+            // not good
+            for(i=0;i<6;i++){
+                x++;
+            }
+
+            // good
+            for (i = 0; i < 6; i++) {
+                x++;
+            }
+
 
     
 
